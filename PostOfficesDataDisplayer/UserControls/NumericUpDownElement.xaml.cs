f36af1cd7fe8d669a6565995c24a6662f929f6b1
ChrisBenua <kpbenua@edu.hse.ru>
2019-02-27 17:22:47 +0300

    Added my own NumericUpDown element and basic stuff

---
+++ PostOfficesDataDisplayer/UserControls/NumericUpDownElement.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PostOfficesDataDisplayer.UserControls.UserControlsViewModel;
+
+namespace PostOfficesDataDisplayer.UserControls
+{
+    /// <summary>
+    /// Interaction logic for NumericUpDownElement.xaml
+    /// </summary>
+    public partial class NumericUpDownElement : UserControl
+    {
+
+        public readonly static DependencyProperty MinimumValueProperty = DependencyProperty.Register("Minimum", typeof(int), 
+            typeof(NumericUpDownElement), new UIPropertyMetadata(0));
+        public readonly static DependencyProperty MaximumValueProperty = DependencyProperty.Register("Maximum", typeof(int), 
+            typeof(NumericUpDownElement), new UIPropertyMetadata(100));
+        public readonly static DependencyProperty InitialValueProperty = DependencyProperty.Register("InitialValue", typeof(int),
+            typeof(NumericUpDownElement), new UIPropertyMetadata(0));
+
+        private NumericUpDownViewModel viewModel;
+
+        public int Maximum
+        {
+            get
+            {
+                return (int)GetValue(MaximumValueProperty);
+            }
+            set
+            {
+                SetValue(MaximumValueProperty, value);
+            }
+        }
+
+        //public int Maximum => ;
+
+        public int Minimum
+        {
+            get
+            {
+                return (int)GetValue(MinimumValueProperty);
+            }
+
+            set
+            {
+                SetValue(MinimumValueProperty, value);
+            }
+        }
+
+        public int InitialValue
+        {
+            get
+            {
+                return (int)GetValue(InitialValueProperty);
+            }
+
+            set
+            {
+                SetValue(InitialValueProperty, value);
+            }
+        }
+
+        public NumericUpDownElement()
+        {
+            viewModel = new NumericUpDownViewModel(Minimum, Maximum, InitialValue);
+            InitializeComponent();
+
+            valueTextBox.SetBinding(TextBox.TextProperty, new Binding()
+            {
+                Source = viewModel,
+                Path = new PropertyPath("Text"),
+                Mode = BindingMode.TwoWay,
+                NotifyOnSourceUpdated = true,
+                NotifyOnTargetUpdated = true,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            });
+
+            increaseButton.Command = viewModel.IncreaseCommand;
+            decreaseButton.Command = viewModel.DecreaseCommand;
+
+            viewModel.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "Text")
+                {
+                    valueTextBox.CaretIndex = viewModel.Text.Length;
+                }
+            };
+        }
+    }
+}
---
+++ PostOfficesDataDisplayer/UserControls/UserControlsViewModel/NumericUpDownViewModel.cs
@@ -0,0 +1,150 @@
+﻿using PostOfficesDataDisplayer.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostOfficesDataDisplayer.UserControls.UserControlsViewModel
+{
+    public class NumericUpDownViewModel: INotifyPropertyChanged
+    {
+        private int _minValue;
+        private int _maxValue;
+
+        public int MinValue
+        {
+            get => _minValue;
+
+            private set
+            {
+                _minValue = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public int MaxValue
+        {
+            get => _maxValue;
+
+            private set
+            {
+                _maxValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _text;
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+
+            set
+            {
+                // _text = value;
+                var tempValue = new string(value.ToCharArray());
+                //validation
+                var arrWithNumbers = value.Where((ch) => ch >= '0' && ch <= '9').SkipWhile(a => a == '0').ToList().
+                    ConvertAll<string>((ch) => new string(ch, 1));
+
+                if (arrWithNumbers.Count != 0)
+                {
+                    tempValue = arrWithNumbers.Aggregate((a, b) => a + b);
+                }
+                else if (value.Length != 0)
+                {
+                    tempValue = _text;
+                }
+                else
+                {
+                    tempValue = MinValue.ToString();
+                }
+
+                _text = tempValue;
+
+                if (!int.TryParse(_text, out _value))
+                {
+                    throw new ArgumentException();
+                }
+
+                Value = _value;
+                
+
+                OnPropertyChanged();
+            }
+        }
+
+        int _value;
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                
+                _value = value;
+
+                _value = Math.Max(MinValue, _value);
+                _value = Math.Min(MaxValue, _value);
+
+                _text = _value.ToString();
+
+
+                OnPropertyChanged("Text");
+
+                OnPropertyChanged();
+            }
+        }
+
+        private RelayCommand _increaseCommand;
+
+        public RelayCommand IncreaseCommand
+        {
+            get
+            {
+                return _increaseCommand ?? (_increaseCommand = new RelayCommand(obj =>
+                {
+                    Value++;
+                }));
+            }
+        }
+
+        private RelayCommand _decreaseCommand;
+
+        public RelayCommand DecreaseCommand
+        {
+            get
+            {
+                return _decreaseCommand ?? (_decreaseCommand = new RelayCommand(obj =>
+                {
+                    Value--;
+                }));
+            }
+        }
+
+        public NumericUpDownViewModel(int minValue, int maxValue, int initialValue)
+        {
+            this.MinValue = minValue;
+            this.MaxValue = maxValue;
+            this.Value = initialValue;
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
